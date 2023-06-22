eae74c1cbb7fbfc88810ea3afa920450a7a29322
Jefferson Nascimento <jnascimento.kstack@veloe.com.br>
2023-06-22 14:21:01 -0300

    First Commit

---
+++ src/test/java/com/bluecode/codeChallange/CodeChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluecode.codeChallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChallangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
